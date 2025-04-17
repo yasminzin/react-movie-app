ba02a96a98272c08d4991fc94eac92284754cee1
yasminzin <yasminaymanzin@gmail.com>
2025-04-18 00:21:48 +0200

    edit navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchResult from "./pages/searchPage/SearchResults";
 import React from "react";
-import Navbar from "./components/Navbar";
+import NavbarComp from "./components/NavbarComp";
 import  styles from './styles/Navbar.module.css'
 const TVShows = lazy(() => import("./pages/TV Shows"));
 const NotFound = lazy(() => import("./pages/NotFound"));
@@ -27,9 +27,9 @@ function App() {
     
     <div className={direction}>
       <Router>
-        <Navbar />
+        {/* <HeaderLayout /> */}
         <Routes>
-          {/* <Route element={<HeaderLayout />}> */}
+          <Route element={<HeaderLayout />}>
             <Route path="/tv" element={<TVShows />} />
             <Route path="/movies/wishlist" element={<MoviesWishlist />}></Route>
             <Route path="/tv/wishlist" element={<TVshowswishlist />}></Route>
@@ -37,7 +37,7 @@ function App() {
             <Route path="/" element={<MoviesList />} />
             <Route path="/movie/:id" element={<MovieDetails />} />
             <Route path="/tv/:id" element={<TVShowDetails />} />
-          {/* </Route> */}
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
--- src/components/HeaderLayout.jsx
+++ src/components/HeaderLayout.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router";
-import Navbar from "./Navbar";
+import NavbarComp from "./NavbarComp";
 
 export default function HeaderLayout() {
   return (
     <div>
-      <Navbar />
+      <NavbarComp />
       <Outlet />
     </div>
   );
